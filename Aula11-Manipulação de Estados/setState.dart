efea4b57aa29296c9625c67869eb30aa4681d7ab
Natalia Costa <102239679+nataliascosta@users.noreply.github.com>
2024-03-24 14:51:44 -0300

    Atualização da aula sobre manipulação de estados. Issue #16

--- Aula11-Manipulação de Estados/setState.dart
+++ Aula11-Manipulação de Estados/setState.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
-      home: MyWidget(), // Adicionando MyWidget como o widget inicial
+      home: MyWidget(),
     );
   }
 }
