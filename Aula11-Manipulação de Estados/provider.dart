bf220acf102158d964cc551a5ae1f8cb11bc1c1a
nataliascosta <102239679+nataliascosta@users.noreply.github.com>
2024-03-30 12:21:41 -0300

    Atualização da aula sobre manipulação de estados. Issue #17

---
+++ Aula11-Manipulação de Estados/provider.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Exemplo com o Provider')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Screen1(),
+            SizedBox(height: 20),
+            Screen2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Defina o Provider
+class CounterProvider extends ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
+// Use o Provider nas telas
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counterProvider = Provider.of<CounterProvider>(context);
+    return Text('Contagem: ${counterProvider.counter}');
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CounterProvider>(
+      builder: (context, counterProvider, child) {
+        return ElevatedButton(
+          onPressed: () => counterProvider.increment(),
+          child: Text('Incrementar'),
+        );
+      },
+    );
+  }
+}
