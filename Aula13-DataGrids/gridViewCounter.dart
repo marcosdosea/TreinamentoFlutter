4350b02178168e2e108b4a87c1b2cb5d42021242
nataliascosta <102239679+nataliascosta@users.noreply.github.com>
2024-04-13 11:36:56 -0300

    Adição de exemplos utilizando Grid Views. Issue #14

---
+++ Aula13-DataGrids/gridViewCounter.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Listas em Grade';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: GridView.count(
+          // Cria uma grade com 2 colunas.
+          crossAxisCount: 2,
+          // Gera 100 widgets que exibam seu índice na lista.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
